5836ecd8ae736d9e1f1f8defcd2061c128535fe3
ShiftUnion <nick.cahill@gmail.com>
2016-10-13 12:20:22 +1300

    redux helper for chrome. See: https://github.com/zalmoxisus/redux-devtools-extension#usage

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,12 +1,15 @@
-import {createStore, applyMiddleware} from 'redux';
-import rootReducer from '../reducers';
-import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
-import thunk from 'redux-thunk';
+import {createStore, applyMiddleware, compose} from "redux";
+import rootReducer from "../reducers";
+import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
+import thunk from "redux-thunk";
 
 export default function configureStore(initialState) {
-  return createStore(
-    rootReducer,
-    initialState,
+
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+  return createStore(rootReducer, initialState, composeEnhancers(
     applyMiddleware(thunk, reduxImmutableStateInvariant())
-  );
+  ));
 }
+
+//added in the redux helper for chrome plugin
