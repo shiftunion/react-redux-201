4f0c02bc2ea53e16b072bd61608737c129d512cc
ShiftUnion <nick.cahill@gmail.com>
2016-10-18 19:37:30 +1300

    Basic formatting

--- tools/build.js
+++ tools/build.js
@@ -2,9 +2,8 @@
 // Allowing console calls below since this is a build file.
 
 /*eslint-disable no-console */
-import webpack from 'webpack';
-import webpackConfig from '../webpack.config.prod';
-import colors from 'colors';
+import webpack from "webpack";
+import webpackConfig from "../webpack.config.prod";
 
 process.env.NODE_ENV = 'production'; // this assures the Babel dev config (for hot reloading) doesn't apply.
 
